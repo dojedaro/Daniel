0e9b518173b40bfc56235a3c423d2e5833dc5435
dojedaro <67751853+dojedaro@users.noreply.github.com>
2025-05-17 12:47:17 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,23 +1,17 @@
 import streamlit as st
 import requests
 
-st.set_page_config(page_title="EN ↔ KO Translator")
+HF_TOKEN = st.secrets["HF_TOKEN"]
 
-# Hugging Face Inference API URLs
+headers = {"Authorization": f"Bearer {HF_TOKEN}"}
+
 API_URLS = {
     "English → Korean": "https://api-inference.huggingface.co/models/Helsinki-NLP/opus-mt-en-ko",
     "Korean → English": "https://api-inference.huggingface.co/models/Helsinki-NLP/opus-mt-ko-en",
 }
 
-
-
-st.title("English ↔ Korean Translator")
-
-direction = st.radio("Select translation direction", ["English → Korean", "Korean → English"])
-text = st.text_area("Enter your text:")
-
 def query(payload, direction):
-    response = requests.post(API_URLS[direction], json={"inputs": payload})
+    response = requests.post(API_URLS[direction], headers=headers, json={"inputs": payload})
     if response.status_code == 200:
         return response.json()[0]["translation_text"]
     else:
